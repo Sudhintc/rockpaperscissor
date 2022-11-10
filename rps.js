e7789404e9739a42784409a7e33dfe218dfcc99d
sudhin tc <tcsudhin@gmail.com>
2022-11-10 20:43:39 +0530

    5rounds

--- rps.js
+++ rps.js
@@ -1,3 +1,5 @@
+let playerScore = 0,computerScore=0;
+let roundWinner='';
 function getComputerChoice(){
     let computerSelection = Math.floor(Math.random()*3+1);
     if(computerSelection==1){
@@ -11,38 +13,51 @@ function getComputerChoice(){
     }
     return computerSelection;
 }
+function playerChoice(){
+    const playerSelection = prompt("enter your choice");
+    return playerSelection;
+    }
 function playRound(playerSelection,computerSelection){
-    if(playerSelection.toLowerCase()===computerSelection){
-        return "no result";
-    }
-    if(computerSelection=="rock"){
-        if(playerSelection=="scissor"){
-            return "computer win";
-        }
-        else if(playerSelection=="paper"){
-            return "player wins";
-        }
-    }
-    if(computerSelection=="paper"){
-        if(playerSelection=="rock"){
-            return "computer wins";
-        }
-        else if(playerSelection=="scissor"){
-            return "player wins";
-        }
-    }
-    if(computerSelection=="scissor"){
-        if(playerSelection=="paper"){
-            return "computer wins";
-        }
-        else if(playerSelection=="rock"){
-            return "player wins";
-        }
-    }
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie'
+      }
+      if (
+        (playerSelection === 'rock' && computerSelection === 'scissor') ||
+        (playerSelection === 'scissor' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+      ) {
+        playerScore++
+        roundWinner = 'player'
+      }
+      if (
+        (computerSelection === 'rock' && playerSelection === 'scissor') ||
+        (computerSelection === 'scissor' && playerSelection === 'paper') ||
+        (computerSelection === 'paper' && playerSelection === 'rock')
+      ) {
+        computerScore++
+        roundWinner = 'computer'
+      }
+      return roundWinner;
+      
 }
+function game(){
+    let playerTurn ='';
+    let computerTurn ='';
+    let roundWinner = '';
+    for(i=0;i<5;i++){
+        playerTurn = playerChoice();
+        computerTurn = getComputerChoice();
+        playRound(playerTurn,computerTurn);
+    }
+    if(playerScore>computerScore){
+        console.log("player wins");
+    }
+    else{
+        console.log("computer wins");
+    }
+    console.log(playerScore);
+    console.log(computerScore);
 
-const playerSelection = prompt("enter your choice");
-console.log(playerSelection);
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
-console.log(computerSelection);
+
+}
+game();
